60a6941a7057f659b38e6695e7bad27ac5193e60
Sarah Mollner <s.a.mollner@gmail.com>
2020-06-01 19:08:08 -0400

    got a card table yay.. again

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,267 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.intro = new System.Windows.Forms.Label();
+            this.jokerBack = new System.Windows.Forms.PictureBox();
+            this.jokerFace = new System.Windows.Forms.PictureBox();
+            this.jokerName = new System.Windows.Forms.Label();
+            this.twoDiamondsface = new System.Windows.Forms.PictureBox();
+            this.twoDiamondsBack = new System.Windows.Forms.PictureBox();
+            this.twoDiamondsName = new System.Windows.Forms.Label();
+            this.tenClubsBack = new System.Windows.Forms.PictureBox();
+            this.tenClubsFace = new System.Windows.Forms.PictureBox();
+            this.tenClubsName = new System.Windows.Forms.Label();
+            this.queenHeartsFace = new System.Windows.Forms.PictureBox();
+            this.queenHeartsBack = new System.Windows.Forms.PictureBox();
+            this.queenHeartsName = new System.Windows.Forms.Label();
+            this.aceSpadeBack = new System.Windows.Forms.PictureBox();
+            this.aceSpadeFace = new System.Windows.Forms.PictureBox();
+            this.aceSpadeName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.jokerBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jokerFace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoDiamondsface)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoDiamondsBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenClubsBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenClubsFace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenHeartsFace)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenHeartsBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadeBack)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadeFace)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // intro
+            // 
+            this.intro.AutoSize = true;
+            this.intro.BackColor = System.Drawing.Color.Transparent;
+            this.intro.Font = new System.Drawing.Font("Playbill", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.intro.ForeColor = System.Drawing.Color.FloralWhite;
+            this.intro.Location = new System.Drawing.Point(194, 18);
+            this.intro.Name = "intro";
+            this.intro.Size = new System.Drawing.Size(396, 65);
+            this.intro.TabIndex = 0;
+            this.intro.Text = "Pick a card, any card...";
+            this.intro.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // jokerBack
+            // 
+            this.jokerBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("jokerBack.BackgroundImage")));
+            this.jokerBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.jokerBack.Location = new System.Drawing.Point(67, 100);
+            this.jokerBack.Name = "jokerBack";
+            this.jokerBack.Size = new System.Drawing.Size(103, 147);
+            this.jokerBack.TabIndex = 1;
+            this.jokerBack.TabStop = false;
+            // 
+            // jokerFace
+            // 
+            this.jokerFace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("jokerFace.BackgroundImage")));
+            this.jokerFace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.jokerFace.Location = new System.Drawing.Point(67, 100);
+            this.jokerFace.Name = "jokerFace";
+            this.jokerFace.Size = new System.Drawing.Size(103, 147);
+            this.jokerFace.TabIndex = 1;
+            this.jokerFace.TabStop = false;
+            // 
+            // jokerName
+            // 
+            this.jokerName.AutoSize = true;
+            this.jokerName.BackColor = System.Drawing.Color.Transparent;
+            this.jokerName.Font = new System.Drawing.Font("Playbill", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.jokerName.ForeColor = System.Drawing.Color.FloralWhite;
+            this.jokerName.Location = new System.Drawing.Point(93, 250);
+            this.jokerName.Name = "jokerName";
+            this.jokerName.Size = new System.Drawing.Size(56, 33);
+            this.jokerName.TabIndex = 2;
+            this.jokerName.Text = "Joker";
+            // 
+            // twoDiamondsface
+            // 
+            this.twoDiamondsface.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("twoDiamondsface.BackgroundImage")));
+            this.twoDiamondsface.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.twoDiamondsface.Location = new System.Drawing.Point(199, 100);
+            this.twoDiamondsface.Name = "twoDiamondsface";
+            this.twoDiamondsface.Size = new System.Drawing.Size(103, 147);
+            this.twoDiamondsface.TabIndex = 1;
+            this.twoDiamondsface.TabStop = false;
+            // 
+            // twoDiamondsBack
+            // 
+            this.twoDiamondsBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("twoDiamondsBack.BackgroundImage")));
+            this.twoDiamondsBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.twoDiamondsBack.Location = new System.Drawing.Point(199, 100);
+            this.twoDiamondsBack.Name = "twoDiamondsBack";
+            this.twoDiamondsBack.Size = new System.Drawing.Size(103, 147);
+            this.twoDiamondsBack.TabIndex = 1;
+            this.twoDiamondsBack.TabStop = false;
+            // 
+            // twoDiamondsName
+            // 
+            this.twoDiamondsName.AutoSize = true;
+            this.twoDiamondsName.BackColor = System.Drawing.Color.Transparent;
+            this.twoDiamondsName.Font = new System.Drawing.Font("Playbill", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.twoDiamondsName.ForeColor = System.Drawing.Color.FloralWhite;
+            this.twoDiamondsName.Location = new System.Drawing.Point(186, 250);
+            this.twoDiamondsName.Name = "twoDiamondsName";
+            this.twoDiamondsName.Size = new System.Drawing.Size(127, 33);
+            this.twoDiamondsName.TabIndex = 2;
+            this.twoDiamondsName.Text = "2 of Diamonds";
+            // 
+            // tenClubsBack
+            // 
+            this.tenClubsBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tenClubsBack.BackgroundImage")));
+            this.tenClubsBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.tenClubsBack.Location = new System.Drawing.Point(337, 100);
+            this.tenClubsBack.Name = "tenClubsBack";
+            this.tenClubsBack.Size = new System.Drawing.Size(103, 147);
+            this.tenClubsBack.TabIndex = 1;
+            this.tenClubsBack.TabStop = false;
+            // 
+            // tenClubsFace
+            // 
+            this.tenClubsFace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("tenClubsFace.BackgroundImage")));
+            this.tenClubsFace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.tenClubsFace.Location = new System.Drawing.Point(337, 100);
+            this.tenClubsFace.Name = "tenClubsFace";
+            this.tenClubsFace.Size = new System.Drawing.Size(103, 147);
+            this.tenClubsFace.TabIndex = 1;
+            this.tenClubsFace.TabStop = false;
+            // 
+            // tenClubsName
+            // 
+            this.tenClubsName.AutoSize = true;
+            this.tenClubsName.BackColor = System.Drawing.Color.Transparent;
+            this.tenClubsName.Font = new System.Drawing.Font("Playbill", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tenClubsName.ForeColor = System.Drawing.Color.FloralWhite;
+            this.tenClubsName.Location = new System.Drawing.Point(335, 250);
+            this.tenClubsName.Name = "tenClubsName";
+            this.tenClubsName.Size = new System.Drawing.Size(108, 33);
+            this.tenClubsName.TabIndex = 2;
+            this.tenClubsName.Text = "10 of Clubs";
+            // 
+            // queenHeartsFace
+            // 
+            this.queenHeartsFace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("queenHeartsFace.BackgroundImage")));
+            this.queenHeartsFace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.queenHeartsFace.Location = new System.Drawing.Point(473, 100);
+            this.queenHeartsFace.Name = "queenHeartsFace";
+            this.queenHeartsFace.Size = new System.Drawing.Size(103, 147);
+            this.queenHeartsFace.TabIndex = 1;
+            this.queenHeartsFace.TabStop = false;
+            // 
+            // queenHeartsBack
+            // 
+            this.queenHeartsBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("queenHeartsBack.BackgroundImage")));
+            this.queenHeartsBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.queenHeartsBack.Location = new System.Drawing.Point(473, 100);
+            this.queenHeartsBack.Name = "queenHeartsBack";
+            this.queenHeartsBack.Size = new System.Drawing.Size(103, 147);
+            this.queenHeartsBack.TabIndex = 1;
+            this.queenHeartsBack.TabStop = false;
+            // 
+            // queenHeartsName
+            // 
+            this.queenHeartsName.AutoSize = true;
+            this.queenHeartsName.BackColor = System.Drawing.Color.Transparent;
+            this.queenHeartsName.Font = new System.Drawing.Font("Playbill", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.queenHeartsName.ForeColor = System.Drawing.Color.FloralWhite;
+            this.queenHeartsName.Location = new System.Drawing.Point(459, 250);
+            this.queenHeartsName.Name = "queenHeartsName";
+            this.queenHeartsName.Size = new System.Drawing.Size(138, 33);
+            this.queenHeartsName.TabIndex = 2;
+            this.queenHeartsName.Text = "Queen of Hearts";
+            // 
+            // aceSpadeBack
+            // 
+            this.aceSpadeBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("aceSpadeBack.BackgroundImage")));
+            this.aceSpadeBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.aceSpadeBack.Location = new System.Drawing.Point(613, 100);
+            this.aceSpadeBack.Name = "aceSpadeBack";
+            this.aceSpadeBack.Size = new System.Drawing.Size(103, 147);
+            this.aceSpadeBack.TabIndex = 1;
+            this.aceSpadeBack.TabStop = false;
+            // 
+            // aceSpadeFace
+            // 
+            this.aceSpadeFace.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("aceSpadeFace.BackgroundImage")));
+            this.aceSpadeFace.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.aceSpadeFace.Location = new System.Drawing.Point(613, 100);
+            this.aceSpadeFace.Name = "aceSpadeFace";
+            this.aceSpadeFace.Size = new System.Drawing.Size(103, 147);
+            this.aceSpadeFace.TabIndex = 1;
+            this.aceSpadeFace.TabStop = false;
+            // 
+            // aceSpadeName
+            // 
+            this.aceSpadeName.AutoSize = true;
+            this.aceSpadeName.BackColor = System.Drawing.Color.Transparent;
+            this.aceSpadeName.Font = new System.Drawing.Font("Playbill", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.aceSpadeName.ForeColor = System.Drawing.Color.FloralWhite;
+            this.aceSpadeName.Location = new System.Drawing.Point(607, 250);
+            this.aceSpadeName.Name = "aceSpadeName";
+            this.aceSpadeName.Size = new System.Drawing.Size(122, 33);
+            this.aceSpadeName.TabIndex = 2;
+            this.aceSpadeName.Text = "Ace of Spades";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.aceSpadeName);
+            this.Controls.Add(this.aceSpadeFace);
+            this.Controls.Add(this.aceSpadeBack);
+            this.Controls.Add(this.queenHeartsName);
+            this.Controls.Add(this.queenHeartsBack);
+            this.Controls.Add(this.queenHeartsFace);
+            this.Controls.Add(this.tenClubsName);
+            this.Controls.Add(this.tenClubsFace);
+            this.Controls.Add(this.tenClubsBack);
+            this.Controls.Add(this.twoDiamondsName);
+            this.Controls.Add(this.twoDiamondsBack);
+            this.Controls.Add(this.twoDiamondsface);
+            this.Controls.Add(this.jokerName);
+            this.Controls.Add(this.jokerFace);
+            this.Controls.Add(this.jokerBack);
+            this.Controls.Add(this.intro);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "cardTable";
+            ((System.ComponentModel.ISupportInitialize)(this.jokerBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.jokerFace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoDiamondsface)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.twoDiamondsBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenClubsBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tenClubsFace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenHeartsFace)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.queenHeartsBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadeBack)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.aceSpadeFace)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label intro;
+        private System.Windows.Forms.PictureBox jokerBack;
+        private System.Windows.Forms.PictureBox jokerFace;
+        private System.Windows.Forms.Label jokerName;
+        private System.Windows.Forms.PictureBox twoDiamondsface;
+        private System.Windows.Forms.PictureBox twoDiamondsBack;
+        private System.Windows.Forms.Label twoDiamondsName;
+        private System.Windows.Forms.PictureBox tenClubsBack;
+        private System.Windows.Forms.PictureBox tenClubsFace;
+        private System.Windows.Forms.Label tenClubsName;
+        private System.Windows.Forms.PictureBox queenHeartsFace;
+        private System.Windows.Forms.PictureBox queenHeartsBack;
+        private System.Windows.Forms.Label queenHeartsName;
+        private System.Windows.Forms.PictureBox aceSpadeBack;
+        private System.Windows.Forms.PictureBox aceSpadeFace;
+        private System.Windows.Forms.Label aceSpadeName;
     }
 }
 
